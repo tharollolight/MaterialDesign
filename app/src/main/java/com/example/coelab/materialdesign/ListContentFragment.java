4dc589ddccf6a4a8401164bf1ca2e1c25492c331
tharollolight <morabatharollo@gmail.com>
2017-10-23 14:43:02 +0200

    Initial commit

---
+++ app/src/main/java/com/example/coelab/materialdesign/ListContentFragment.java
@@ -0,0 +1,102 @@
+package com.example.coelab.materialdesign;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by coeLab on 2017/10/17.
+ */
+
+public class ListContentFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        RecyclerView recyclerView = (RecyclerView) inflater.inflate(R.layout.recycler_view,container,false);
+        ContentAdapter adapter = new ContentAdapter(recyclerView.getContext());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return recyclerView;
+    }
+
+    //
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ImageView avator;
+        public TextView name;
+        public TextView description;
+
+        public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.activity_list_content_fragment, parent, false));
+            avator = (ImageView) itemView.findViewById(R.id.list_avator);
+            name = (TextView) itemView.findViewById(R.id.list_title);
+            description = (TextView) itemView.findViewById(R.id.list_desc);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Context context = v.getContext();
+                    Intent intent = new Intent(context,DetailActivity.class);
+                    context.startActivity(intent);
+                }
+            });
+
+        }
+    }
+
+    public static class ContentAdapter extends RecyclerView.Adapter<ViewHolder>{
+
+        // Set number of thelist in RecyclerView
+        private static  final int Lenth = 18;
+
+        private final String[] mPlaces;
+        private final String[] mPlaceDesc;
+        private final Drawable[] mPlaceAvators;
+
+        public ContentAdapter(Context context){
+            Resources resources = context.getResources();
+            mPlaces = resources.getStringArray(R.array.places);
+            mPlaceDesc = resources.getStringArray(R.array.place_desc);
+            TypedArray a = resources.obtainTypedArray(R.array.place_avator);
+            mPlaceAvators = new Drawable[a.length()];
+
+            for(int i = 0; i < mPlaceAvators.length; i++){
+                mPlaceAvators[i] = a.getDrawable(i);
+            }
+            a.recycle();
+
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new ViewHolder(LayoutInflater.from(parent.getContext()), parent);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+
+            holder.avator.setImageDrawable(mPlaceAvators[position % mPlaceAvators.length]);
+            holder.name.setText(mPlaces[position % mPlaces.length]);
+            holder.description.setText(mPlaceDesc[position % mPlaceDesc.length]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return Lenth;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/coelab/materialdesign/TileContentFragment.java
@@ -0,0 +1,97 @@
+package com.example.coelab.materialdesign;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by coeLab on 2017/10/17.
+ */
+
+public class TileContentFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        RecyclerView recyclerView = (RecyclerView) inflater.inflate(R.layout.recycler_view,container,false);
+        ContentAdapter adapter = new ContentAdapter(recyclerView.getContext());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+
+        // Set Padding for Tiles (not needed for Cards/Lists)
+        int tilePadding = getResources().getDimensionPixelOffset(R.dimen.tile_padding);
+        recyclerView.setPadding(tilePadding, tilePadding, tilePadding, tilePadding);
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+
+        return recyclerView;
+    }
+
+    //
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView picture;
+        public TextView name;
+
+        public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.activity_tile_content_fragment, parent, false));
+            picture = (ImageView) itemView.findViewById(R.id.tile_picture);
+            name = (TextView) itemView.findViewById(R.id.tile_title);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Context context = v.getContext();
+                    Intent intent = new Intent(context,DetailActivity.class);
+                    context.startActivity(intent);
+                }
+            });
+        }
+    }
+
+    public static class ContentAdapter extends RecyclerView.Adapter<ViewHolder>{
+
+        // Set number of thelist in RecyclerView
+        private static  final int Lenth = 18;
+
+        private final String[] mPlaces;
+        private final Drawable[] mPlacePictures;
+
+        public ContentAdapter(Context context){
+            Resources resources = context.getResources();
+            mPlaces = resources.getStringArray(R.array.places);
+            TypedArray a = resources.obtainTypedArray(R.array.places_picture);
+            mPlacePictures = new Drawable[a.length()];
+            for (int i = 0; i < mPlacePictures.length; i++) {
+                mPlacePictures[i] = a.getDrawable(i);
+            }
+            a.recycle();
+
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new ViewHolder(LayoutInflater.from(parent.getContext()), parent);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+
+            holder.picture.setImageDrawable(mPlacePictures[position % mPlacePictures.length]);
+            holder.name.setText(mPlaces[position % mPlaces.length]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return Lenth;
+        }
+    }
+}
